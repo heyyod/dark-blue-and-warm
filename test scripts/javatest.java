0183002570006bb32d6edd27a207350f12b8da65
Konstantinos Damaskinos <kondamask@gmail.com>
2020-12-30 19:41:55 +0200

    fixed python functions

---
+++ test scripts/javatest.java
@@ -0,0 +1,12 @@
+public class javatest {
+    class myclass {
+        public int a = 10;
+    }
+
+    public static void main(String[] args) {
+        myclass test;
+        test.a = 5;
+
+        int b;
+    }
+}
